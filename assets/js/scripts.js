5642e12e58deb66836f47539deb4790f5353ef57
Julia Bode <julia.bode@hmmh.de>
2014-09-30 01:22:53 +0200

    Remove scripts.js from gitignore.

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+jQuery(document).ready( function($) {
+
+    function sticky() {
+        var window_top          = $(window).scrollTop(),
+            top_position        = 151,
+            element_to_stick    = $('header');
+        
+        if (window_top > top_position) {console.log('sticky');
+            element_to_stick.addClass('sticky');
+        } else {
+            element_to_stick.removeClass('sticky');
+        }
+    }
+
+    $(window).scroll(sticky);
+    sticky();
+
+});
