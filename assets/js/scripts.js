0eb393e14b682c9db2bbfe8994af235e558d5530
Julia Bode <julia.bode@hmmh.de>
2015-02-26 10:41:20 +0100

    Fix ie bugs.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,4 +40,37 @@ jQuery(document).ready( function($) {
         jQuery('div.js-event-highlighter[data-event-id="' + eventId + '"]').parent('.gce-feed').removeClass('active');
     });
 
+    /**
+     * detect IE
+     * returns version of IE or false, if browser is not Internet Explorer
+     */
+    function detectIE() {
+        var ua = window.navigator.userAgent;
+
+        var msie = ua.indexOf('MSIE ');
+        if (msie > 0) {
+            // IE 10 or older => return version number
+            return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+        }
+
+        var trident = ua.indexOf('Trident/');
+        if (trident > 0) {
+            // IE 11 => return version number
+            var rv = ua.indexOf('rv:');
+            return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+        }
+
+        var edge = ua.indexOf('Edge/');
+        if (edge > 0) {
+           // IE 12 => return version number
+           return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+        }
+
+        // other browser
+        return false;
+    }
+
+    var ieClass = detectIE() || '';
+    if (ieClass) { jQuery('html').addClass('ie'); };
+
 });
