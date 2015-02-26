07d6f9130ec12c5d2fb5d4a824e686156db3b678
Julia Bode <julia.bode@hmmh.de>
2015-02-26 11:36:40 +0100

    Style 404 page.

--- 404.php
+++ 404.php
@@ -1,13 +1,17 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<div class="row">
+    <div class="small-12 column">
 
-<div class="alert alert-warning">
-  <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+        <div class="alert alert-warning white-bg vines">
+            <?php get_template_part('templates/page', 'header'); ?>
+            <?php _e('Sorry, but the page you were trying to view does not exist.', 'roots'); ?>
+
+            <p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
+            <ul>
+              <li><?php _e('a mistyped address', 'roots'); ?></li>
+              <li><?php _e('an out-of-date link', 'roots'); ?></li>
+            </ul>
+        </div>
+
+    </div>
+
 </div>
-
-<p><?php _e('It looks like this was the result of either:', 'roots'); ?></p>
-<ul>
-  <li><?php _e('a mistyped address', 'roots'); ?></li>
-  <li><?php _e('an out-of-date link', 'roots'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
